f1282dc7ae30e2afc725916385507ffebb995e95
kreative358 <105795822+kreative358@users.noreply.github.com>
2024-12-03 19:39:26 +0100

    5 commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,33 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useGlobalContext } from "./context";
-// import { FullScreen, useFullScreenHandle } from "react-full-screen";
-// import "./fullscreen.css";
-import { ImageGroup, Image } from "react-fullscreen-image";
-import { SRLWrapper } from "simple-react-lightbox";
-import SimpleReactLightbox from "simple-react-lightbox";
-
-// const url = `https://api.unsplash.com/search/photos?client_id=${
-//   import.meta.env.VITE_API_KEY
-// }`;
-// https://unsplash.com/documentation#search-photos
-// 672730
-// AGDv1AKz5PVGE17gu2sAf3-pEvukMPubWkKufxJfiZM
-// c2RDz0J7V8jfqBV71tF89Fks_v6Ae8Gyxzgz0iuEs6M
-// ${import.meta.env.VITE_API_KEY}`
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Download from "yet-another-react-lightbox/plugins/download";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/styles.css";
+import { RowsPhotoAlbum } from "react-photo-album";
 
 const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_API_KEY
 }`;
 
-// const url =
-//   "https://api.unsplash.com/search/photos?client_id=AGDv1AKz5PVGE17gu2sAf3-pEvukMPubWkKufxJfiZM&page=1&per_page=12";
-
-// div.ReactGridGallery_tile-viewport > img
-// body > div.ReactModalPortal > div > div > div > div.ril-inner.ril__inner > img.ril-image-current.ril__image
+const inline = {
+  style: {
+    width: "100%",
+    maxWidth: "900px",
+    aspectRatio: "3 / 2",
+    margin: "0 auto",
+  },
+};
 
 const Gallery = () => {
   const { searchTerm, searchPage } = useGlobalContext();
+  const [index, setIndex] = useState(-1);
+
   const response = useQuery({
     // queryKey: ["images"],
     queryKey: ["images", searchTerm, searchPage],
@@ -63,49 +66,47 @@ const Gallery = () => {
       </section>
     );
   }
-  // gridTemplateColumns "repeat(auto-fit, minmax(300px, 1fr))"
-  return (
-    // <div className="container">
-    //   <ImageGroup>
-    //     <ul
-    //       style={{
-    //         gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-    //       }}
-    //       className="images"
-    //     >
-    //       {results.map((item) => {
-    //         const url = item?.urls?.regular;
-    //         return (
-    //           <li key={item.id}>
-    //             <Image src={url} key={item.id} alt={item.alt_description} />
-    //           </li>
-    //         );
-    //       })}
-    //     </ul>
-    //   </ImageGroup>
-    // </div>
 
-    <div className="container">
-      <SimpleReactLightbox>
-        <SRLWrapper>
-          <ul
-            style={{
-              gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-            }}
-            className="images"
-          >
-            {results.map((item) => {
-              const url = item?.urls?.regular;
-              return (
-                <li key={item.id}>
-                  <img src={url} key={item.id} alt={item.alt_description} />
-                </li>
-              );
-            })}
-          </ul>
-        </SRLWrapper>
-      </SimpleReactLightbox>
-    </div>
+
+const slides = results.map((item, index) => {
+  
+})
+
+  return (
+    <>
+   <div className="container">
+      <ul
+          style={{
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+          }}
+          className="images"
+        >
+          {results.map((item, id) => {
+            const url = item?.urls?.regular;
+            // const index = item.id
+            return (
+              <li key={id} >
+                <img src={url} key={id} alt={item.alt_description} onClick={() => setIndex(id)}/>
+              </li>
+            );
+          })}
+        </ul>
+
+        </div>
+
+      <Lightbox
+        index={index}
+        slides={results.map((item, id) => ({
+          src: item?.urls?.regular,
+        }))}
+        inline={inline}
+        open={index >= 0}
+        // open={index}
+        close={() => setIndex(-1)}
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom, Download]}
+      />
+
+    </>
   );
 };
 export default Gallery;
